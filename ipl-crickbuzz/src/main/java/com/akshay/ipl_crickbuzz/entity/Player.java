624b4f67c8afcd5db32cd2ea8e05e83bf32618b0
Akshay Kawade <kawadeakshay93@gmail.com>
2026-08-15 13:59:52 +0530

    feat: configure MySQL and create Player entity

---
+++ ipl-crickbuzz/src/main/java/com/akshay/ipl_crickbuzz/entity/Player.java
@@ -0,0 +1,88 @@
+package com.akshay.ipl_crickbuzz.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class Player {
+	
+	@Id
+	private int playerId;
+	private int jerseyNumber;
+	private String playerName;
+	private int runs;
+	private int wickets;
+	private String teamName;
+	private String specialization;
+	
+	public Player() {
+		
+	}
+
+	public int getPlayerId() {
+		return playerId;
+	}
+
+	public void setPlayerId(int playerId) {
+		this.playerId = playerId;
+	}
+
+	public int getJerseyNumber() {
+		return jerseyNumber;
+	}
+
+	public void setJerseyNumber(int jerseyNumber) {
+		this.jerseyNumber = jerseyNumber;
+	}
+
+	public String getPlayerName() {
+		return playerName;
+	}
+
+	public void setPlayerName(String playerName) {
+		this.playerName = playerName;
+	}
+
+	public int getRuns() {
+		return runs;
+	}
+
+	public void setRuns(int runs) {
+		this.runs = runs;
+	}
+
+	public int getWickets() {
+		return wickets;
+	}
+
+	public void setWickets(int wickets) {
+		this.wickets = wickets;
+	}
+
+	public String getTeamName() {
+		return teamName;
+	}
+
+	public void setTeamName(String teamName) {
+		this.teamName = teamName;
+	}
+
+	public String getSpecialization() {
+		return specialization;
+	}
+
+	public void setSpecialization(String specialization) {
+		this.specialization = specialization;
+	}
+
+	@Override
+	public String toString() {
+		return "Player [playerId=" + playerId + ", jerseyNumber=" + jerseyNumber + ", playerName=" + playerName
+				+ ", runs=" + runs + ", wickets=" + wickets + ", teamName=" + teamName + ", specialization="
+				+ specialization + ", getPlayerId()=" + getPlayerId() + ", getJerseyNumber()=" + getJerseyNumber()
+				+ ", getPlayerName()=" + getPlayerName() + ", getRuns()=" + getRuns() + ", getWickets()=" + getWickets()
+				+ ", getTeamName()=" + getTeamName() + ", getSpecialization()=" + getSpecialization() + ", getClass()="
+				+ getClass() + ", hashCode()=" + hashCode() + ", toString()=" + super.toString() + "]";
+	}
+	
+}
