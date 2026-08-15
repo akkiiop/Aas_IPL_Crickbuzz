458f3ec91c1ede19d2c9b9c77d51423c79f9f551
Akshay Kawade <kawadeakshay93@gmail.com>
2026-08-15 12:52:42 +0530

    chore: initialize IPL Crickbuzz Spring Boot project

---
+++ ipl-crickbuzz/src/main/java/com/akshay/ipl_crickbuzz/IplCrickbuzzApplication.java
@@ -0,0 +1,13 @@
+package com.akshay.ipl_crickbuzz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IplCrickbuzzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IplCrickbuzzApplication.class, args);
+	}
+
+}
---
+++ ipl-crickbuzz/src/test/java/com/akshay/ipl_crickbuzz/IplCrickbuzzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akshay.ipl_crickbuzz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IplCrickbuzzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
